2bcd333ff898046343942550c8a1739b2bf42ab3
lesley <lesleybanadzem2017@gmail.com>
2020-11-02 11:53:12 -0800

    queried all buttons from the html file and save them to variables

---
+++ main.js
@@ -0,0 +1,5 @@
+const all_numbers = document.querySelectorAll("[data-integer]");
+const operand = document.querySelectorAll("[data-operand]");
+const clear_all = document.querySelector("[data-clear]");
+const equal = document.querySelector("[data-equals]");
+const integer_sign = document.querySelectorAll("[data-int-sign]");
