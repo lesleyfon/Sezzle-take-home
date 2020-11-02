9bc9a08f62ef77f96e12d9c0f154f8d6618cd4f3
lesley <lesleybanadzem2017@gmail.com>
2020-11-02 12:15:12 -0800

    created a calculator class and some methods and properties for the calculator class

--- main.js
+++ main.js
@@ -1,5 +1,44 @@
-const all_numbers = document.querySelectorAll("[data-integer]");
+const allNumbers = document.querySelectorAll("[data-integer]");
 const operand = document.querySelectorAll("[data-operand]");
-const clear_all = document.querySelector("[data-clear]");
+const clearAll = document.querySelector("[data-clear]");
 const equal = document.querySelector("[data-equals]");
-const integer_sign = document.querySelectorAll("[data-int-sign]");
+const integerSign = document.querySelectorAll("[data-int-sign]");
+
+class Calculator {
+	constructor() {
+		this.firstInt = null;
+		this.secondInt = null;
+		this.total = null;
+		this.topDisplay = document.getElementById("top-display");
+		this.bottomDisplay = document.getElementById("bottom-display");
+		this.operationSign = "";
+	}
+
+	clearTop() {
+		this.topDisplay.innerText = "";
+	}
+	clearBottom() {
+		this.bottomDisplay.innerText = "";
+	}
+	clearAll() {
+		this.clearTop();
+		this.clearBottom();
+	}
+	updateDisplay(number) {
+		this.bottomDisplay.innerText += String(number);
+	}
+}
+
+// Clear all field
+
+const calculator = new Calculator();
+
+clearAll.addEventListener("click", () => {
+	calculator.clearAll();
+});
+
+allNumbers.forEach((number) => {
+	number.addEventListener("click", (event) => {
+		calculator.updateDisplay(event.target.innerText);
+	});
+});
