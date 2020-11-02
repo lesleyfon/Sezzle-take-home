02054216c6c84b46c6e43d6ce1f1c1fe25b4327a
lesley <lesleybanadzem2017@gmail.com>
2020-11-02 12:34:37 -0800

    Wrote code to update and clear top display as well as append the first int and sign when someone enters a number

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const allNumbers = document.querySelectorAll("[data-integer]");
-const operand = document.querySelectorAll("[data-operand]");
+const operands = document.querySelectorAll("[data-operand]");
 const clearAll = document.querySelector("[data-clear]");
 const equal = document.querySelector("[data-equals]");
 const integerSign = document.querySelectorAll("[data-int-sign]");
@@ -24,21 +24,37 @@ class Calculator {
 		this.clearTop();
 		this.clearBottom();
 	}
-	updateDisplay(number) {
+	updateButtonDisplay(number) {
 		this.bottomDisplay.innerText += String(number);
+	}
+	setOperand(sign) {
+		this.firstInt = this.bottomDisplay.innerText;
+		this.operationSign = sign;
+		this.clearBottom();
+
+		if (!!this.firstInt || this.firstInt === 0) {
+			this.topDisplay.innerText = `${this.firstInt} ${sign}`;
+		}
 	}
 }
 
-// Clear all field
-
 const calculator = new Calculator();
 
+// Clear all field
 clearAll.addEventListener("click", () => {
 	calculator.clearAll();
 });
 
+// Click event for numbers
 allNumbers.forEach((number) => {
 	number.addEventListener("click", (event) => {
-		calculator.updateDisplay(event.target.innerText);
+		calculator.updateButtonDisplay(event.target.innerText);
+	});
+});
+
+// Click events for operands
+operands.forEach((operand) => {
+	operand.addEventListener("click", (event) => {
+		calculator.setOperand(event.target.innerText);
 	});
 });
